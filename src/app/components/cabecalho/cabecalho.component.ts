9fb6c1e5e18dabec18ea56b9c6f752b027f0fee0
teoitinga <teo.itinga@gmail.com>
2020-08-17 18:31:17 -0300

    login de usuario

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/api.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-cabecalho',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabecalhoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private apiservice: ApiService
+  ) { }
 
   ngOnInit(): void {
+  }
+  get usuario():BehaviorSubject<UsuarioModel>{
+    return this.apiservice.usuario;
+  }
+  logout(){
+    console.log('fazendo logout');
+    this.apiservice.logout();
+  }
+  get roleadmin(): Observable<boolean>{
+    return new Observable<boolean>(observer => {
+      observer.next(this.apiservice.roleadmin);
+    });
+
+  }
+  get roletecnico(): Observable<boolean>{
+    return new Observable<boolean>(observer => {
+      observer.next(this.apiservice.roletecnico);
+    });
+
   }
 
 }
--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import * as AppUtils from '../shared/app.utils';
 import { CredencialModel } from '../models/credencial.model';
 import { HttpParams, HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { RouteConfigLoadEnd, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,11 @@ export class ApiService {
   }
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private route: Router
+  ) { 
+
+  }
 
   login(user: CredencialModel): Observable <any> {
 
@@ -27,15 +31,46 @@ export class ApiService {
   }
   registerToken(token:string){
     window.localStorage.setItem(AppUtils.TOKEN, token);
+    this.usuario.next( this.jwtDecode(this.getAccessToken()))
   }
   jwtDecode(token:string): UsuarioModel {
     let usuario: UsuarioModel;
-    usuario = JSON.parse(window.atob(token.split('.')[1]));
+    try{
+      usuario = JSON.parse(window.atob(token.split('.')[1]));
+      return usuario;
+    }catch(err){
+      return null;
+    }
 
-    return usuario;
   }
   getAccessToken():string {
     return window.localStorage[AppUtils.TOKEN];
+  }
+  get usuario():BehaviorSubject<UsuarioModel>{
+    return new BehaviorSubject(
+      this.jwtDecode(this.getAccessToken())
+    );
+  }
+  logout(){
+    window.localStorage.removeItem(AppUtils.TOKEN);
+    this.usuario.next( this.jwtDecode(this.getAccessToken()) )
+    this.route.navigate(['/']);
+  }
+  get roleadmin():boolean{
+    try{
+      console.log("role: "+this.jwtDecode(this.getAccessToken()).role);
+      return this.jwtDecode(this.getAccessToken()).role === 'ADMIN'? true : false
+    }catch(err){
+      return false;
+    }
+  }
+  get roletecnico():boolean{
+    try{
+      console.log("role: "+this.jwtDecode(this.getAccessToken()).role);
+      return this.jwtDecode(this.getAccessToken()).role === 'TECNICO'? true : false
+    }catch(err){
+      return false;
+    }
   }
 
 }
